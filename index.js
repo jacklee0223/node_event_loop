dc6818e78182a8e6f6ab5b4fa84efc2fcc2edc8d
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-22 20:45:23 -0600

    implement clustering

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const cluster = require('cluster');
 
-console.log(cluster.isMaster);
-const express = require('express');
-const app = express();
+if (cluster.isMaster) {
+  cluster.fork();
+} else {
+  const express = require('express');
+  const app = express();
 
-function doWork(duration) {
-  const start = Date.now();
+  function doWork(duration) {
+    const start = Date.now();
 
-  while (Date.now() - start < duration) {}
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send(`Hey there`);
+  });
+
+  app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send(`Hey there`);
-});
-
-app.listen(3000);
