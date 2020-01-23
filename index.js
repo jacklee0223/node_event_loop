bf235c9d2b53b78846d7fd04562249929b9d375d
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-22 20:42:05 -0600

    implement nodemon script and cluster

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const cluster = require('cluster');
+
+console.log(cluster.isMaster);
 const express = require('express');
 const app = express();
 
+function doWork(duration) {
+  const start = Date.now();
+
+  while (Date.now() - start < duration) {}
+}
+
 app.get('/', (req, res) => {
-  res.send('Hi there');
+  doWork(5000);
+  res.send(`Hey there`);
 });
 
 app.listen(3000);
