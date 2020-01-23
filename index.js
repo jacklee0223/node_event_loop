8fe28934e7d0334b5e4b798d38de9a799dd6266c
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-22 23:02:22 -0600

    test out clustering

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
 
 if (cluster.isMaster) {
   cluster.fork();
+  cluster.fork();
 } else {
   const express = require('express');
   const app = express();
-
-  function doWork(duration) {
-    const start = Date.now();
-
-    while (Date.now() - start < duration) {}
-  }
+  const crypto = require('crypto');
 
   app.get('/', (req, res) => {
-    doWork(5000);
-    res.send(`Hey there`);
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send(`Hey there`);
+    });
   });
 
   app.get('/fast', (req, res) => {
